118aef4e8aca9d5753cfae976a43d99448f3a172
Fernando <fernandoq123@gmail.com>
2026-02-26 08:18:17 +1100

    Adding auth functionality

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,6 +1,105 @@
+import { useContext, useState } from "react"
+import { useForm } from "react-hook-form";
+import { AuthContext } from "../context/AuthContext";
+import { useNavigate } from "react-router-dom";
 
 export default function Auth(){
+    const [mode, setMode] = useState("singup");
+    const [error, setError] = useState(null);
+    const navigate = useNavigate();
+    const {signUp, user, logout, login} = useContext(AuthContext)
+    const {
+        register, 
+        handleSubmit, 
+        formState:{errors}} = useForm();
+
+    function onSubmit(data){
+        setError(null);
+
+        let result;
+        if(mode === "singup"){
+            result = signUp(data.email, data.password);
+        }else{
+            result = login(data.email, data.password);
+        }
+
+        if(!result.success){
+            setError(result.error);
+            return;
+        }
+
+        navigate("/");
+    }
     return (
-        <div>Auth</div>
+        <div className="page">
+            <div className="container">
+                <div className="auth-container">
+                    {user && <p>User logged in: {user.email}</p>}
+                    <button onClick={() => logout()}>LogOut</button>
+                    <h1 className="page-title">
+                        {mode =="singup" ?"Sing Up":"Login"}
+                    </h1>
+                    <form className="auth-form" onSubmit={handleSubmit(onSubmit)}>
+                        {error && (
+                            <div className="error-message">
+                                {error}
+                            </div>
+                        )}
+                        
+                        <div className="form-group">
+                            <label className="form-label" htmlFor="email">
+                                Email
+                            </label>
+                            <input className="form-input" type="email" id="email" 
+                                {...register('email', { required: "Email is required"})} />
+                            {errors.email && (
+                                <span className="form-error">
+                                    {errors.email.message}
+                                </span>
+                            )}
+                        </div>
+                        <div className="form-group">
+                            <label className="form-label" htmlFor="password">
+                                Password
+                            </label>
+                            <input className="form-input" type="password" id="password"
+                                {...register('password', {
+                                     required: "Password is required",
+                                     minLength:{
+                                        value: 6,
+                                        message: "Password must be at least 6 characters"
+                                     },
+                                     maxLength:{
+                                        value: 12,
+                                        message: "Password must be less than 12 characters"
+                                     },
+                                     })} />
+                            {errors.password && (
+                                <span className="form-error">
+                                    {errors.password.message}
+                                </span>
+                            )}    
+                        </div>
+
+                        <button type="submit" className="btn btn-primary btn-large">
+                           {mode =="singup" ?"Sing Up":"Login"}
+                        </button>
+                    </form> 
+
+                    <div className="auth-switch">
+                        {mode === "singup" ?
+                        (<p>
+                            Alredy have an account? <span className="auth-link" onClick={() => setMode("login")} >Login</span>
+                        </p>)
+                        :(<p>
+                            Don't have an account? <span className="auth-link" onClick={() => setMode("singup")}>Sing Up</span>
+                        </p>
+
+                        )
+                        }
+                    </div>                   
+                </div>
+            </div>  
+        </div>
     )
 }
